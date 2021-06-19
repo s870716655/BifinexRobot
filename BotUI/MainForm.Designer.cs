dc030008a77e5d15ff3f2fcd07fee7101db6a6b8
s870716655 <s870716655@gmail.com>
2021-06-20 05:29:50 +0800

    UI新增BTCUSD價格顯示元件

--- BotUI/MainForm.Designer.cs
+++ BotUI/MainForm.Designer.cs
@@ -28,13 +28,34 @@ namespace BTCTrandBot
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Lab_PriceBTCUSD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Lab_PriceBTCUSD
+            // 
+            this.Lab_PriceBTCUSD.BackColor = System.Drawing.Color.White;
+            this.Lab_PriceBTCUSD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lab_PriceBTCUSD.ForeColor = System.Drawing.Color.Black;
+            this.Lab_PriceBTCUSD.Location = new System.Drawing.Point(41, 27);
+            this.Lab_PriceBTCUSD.Name = "Lab_PriceBTCUSD";
+            this.Lab_PriceBTCUSD.Size = new System.Drawing.Size(100, 20);
+            this.Lab_PriceBTCUSD.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Lab_PriceBTCUSD);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Lab_PriceBTCUSD;
     }
 }
 

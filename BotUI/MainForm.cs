0457ab0e7273944454ba1dbb15f8fb0125294972
s870716655 <s870716655@gmail.com>
2021-06-20 06:05:32 +0800

    新增 BTCUSD價格顯示功能

--- BotUI/MainForm.cs
+++ BotUI/MainForm.cs
@@ -22,10 +22,10 @@ namespace BTCTrandBot
             InitClient();
 
             // Initailize timer
-            m_Timer_1000ms = new System.Windows.Forms.Timer();
-            m_Timer_1000ms.Interval = 1000;
-            m_Timer_1000ms.Tick += Timer_1000ms_Tick;
-            m_Timer_1000ms.Enabled = true;
+            m_Timer_5000ms = new System.Windows.Forms.Timer();
+            m_Timer_5000ms.Interval = 5000;
+            m_Timer_5000ms.Tick += Timer_5000ms_Tick;
+            m_Timer_5000ms.Enabled = true;
 
             m_Timer_500ms = new System.Windows.Forms.Timer();
             m_Timer_500ms.Interval = 500;
@@ -34,7 +34,7 @@ namespace BTCTrandBot
         }
 
         BitfinexClient m_Client;
-        System.Windows.Forms.Timer m_Timer_1000ms;
+        System.Windows.Forms.Timer m_Timer_5000ms;
         System.Windows.Forms.Timer m_Timer_500ms;
 
         void InitClient()
@@ -48,7 +48,6 @@ namespace BTCTrandBot
                 return;
             }
 
-            WebCallResult<IEnumerable<BitfinexSymbolOverview>> BTCUSDInfo = m_Client.GetTickerAsync(new CancellationToken(), "tBTCUSD").Result;
             //WebCallResult<IEnumerable<BitfinexCurrency>> CoinType = m_Client.GetCurrenciesAsync().Result;
             //Task <WebCallResult<IEnumerable<BitfinexCurrency>>> GetCoinTypeTask = m_Client.GetCurrenciesAsync();
             //GetCoinTypeTask.Start
@@ -67,13 +66,22 @@ namespace BTCTrandBot
             return true;
         }
 
-        void Timer_1000ms_Tick(object sender, EventArgs e)
+        void Timer_5000ms_Tick(object sender, EventArgs e)
         {
             m_Client.GetTickerAsync(new CancellationToken(), "tBTCUSD");
         }
 
         void Timer_500ms_Tick(object sender, EventArgs e)
         {
+            WebCallResult<IEnumerable<BitfinexSymbolOverview>> CallResult = m_Client.GetTickerAsync(new CancellationToken(), "tBTCUSD").Result;
+            IEnumerator<BitfinexSymbolOverview> BTCUSDInfo = CallResult.Data.GetEnumerator();
+            
+            // Get first element
+            BTCUSDInfo.MoveNext();
+
+            // Update price
+            string szPrice_BTCUSD = BTCUSDInfo.Current.LastPrice.ToString("0");
+            Lab_PriceBTCUSD.Text = szPrice_BTCUSD;
         }
     }
 }
